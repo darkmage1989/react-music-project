ac7422bba66d13459b955345b71000d049aa3c73
darkmage1989 <darkmage1989@gmail.com>
2023-06-06 19:34:32 +0300

    для наставниа

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import './css/style.css'
+import Wrapper from './wrapper'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Wrapper />
   </React.StrictMode>
 )
 
---
+++ src/wrapper.js
@@ -0,0 +1,14 @@
+function wrapper() {
+  return <div className="wrapper">
+    <div className="container">
+      <main className="main">
+        <nav className="main__nav nav"></nav>
+        <div className="main__centerblock centerblock"></div>
+        <div className="main__sidebar sidebar"></div>
+      </main>
+      <footer className="footer"></footer>
+    </div>
+  </div>
+}
+
+export default wrapper
