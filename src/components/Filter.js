e795da7f2aac7111eb6b6c1d479297fff9845919
darkmage1989 <darkmage1989@gmail.com>
2023-06-12 22:02:25 +0300

    тест

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,58 @@
+import { useState } from 'react'
+
 function Filter() {
+  const [visibleFilter, setVisibleFilter] = useState(null)
+  const toggleVisibleFilter = (filter) => {
+    setVisibleFilter(visibleFilter === filter ? null : filter)
+  }
   return (
     <div className="centerblock__filter filter">
       <div className="filter__title">Искать по:</div>
-      <div className="filter__button button-author _btn-text">исполнителю</div>
-      <div className="filter__button button-year _btn-text">году выпуска</div>
-      <div className="filter__button button-genre _btn-text">жанру</div>
+      <div
+        onClick={() => toggleVisibleFilter('author')}
+        className="filter__button button-author _btn-text"
+      >
+        исполнителю
+      </div>
+      {visibleFilter === 'author' && (
+        <div className="test">
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+        </div>
+      )}
+      <div
+        onClick={() => toggleVisibleFilter('year')}
+        className="filter__button button-year _btn-text"
+      >
+        году выпуска
+      </div>
+      {visibleFilter === 'year' && (
+        <div className="test">
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+        </div>
+      )}
+      <div
+        onClick={() => toggleVisibleFilter('genre')}
+        className="filter__button button-genre _btn-text"
+      >
+        жанру
+      </div>
+      {visibleFilter === 'genre' && (
+        <div className="test">
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+          <span>dasfdsgdfgdff</span>
+        </div>
+      )}
     </div>
   )
 }
